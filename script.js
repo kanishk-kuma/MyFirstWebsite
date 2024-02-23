cc168c58d2d02323306e9fa62c8a3839888a14c3
kanishk-kuma <86834559+kanishk-kuma@users.noreply.github.com>
2024-02-23 12:27:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+
+
+}
+function seterror(id, error){
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validate(){
+    let returnval = true;
+    clearErrors();
+
+    //perform validation and if validation fails, set the value of returnval to false
+    let name = document.forms['mainForm']["fname"].value;
+    if (name.length<5){
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+
+    if (name.length == 0){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+    let email = document.forms['mainForm']["femail"].value;
+    if (email.length>15){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+
+    let phone = document.forms['mainForm']["fphone"].value;
+    if (phone.length != 10){
+        seterror("phone", "*Phone number should be of 10 digits!");
+        returnval = false;
+    }
+
+    let password = document.forms['mainForm']["fpassword"].value;
+    if (password.length < 6){
+
+        // Quiz: create a logic to allow only those passwords which contain atleast one letter, one number and one special character and one uppercase letter
+        seterror("password", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+    return returnval;
+}
